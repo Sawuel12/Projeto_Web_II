9c8a369d06ab0807357e178a6b6635433d822421
Sawuel12 <usamuelfilho.1@gmail.com>
2026-08-28 21:07:47 -0300

    Estrutura: corrigido

---
+++ src/test/java/br/ueg/trindade/projeto_web2_fullstack/ProjetoWeb2FullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ueg.trindade.projeto_web2_fullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWeb2FullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
